9cf9e7bd035295f2347b576ef54f744a43b6df4c
tester7797 <testermail7797@gmail.com>
2024-02-29 12:56:41 +0530

    added demo.py file

---
+++ demo.py
@@ -0,0 +1,13 @@
+
+
+print('--reverse the number--')
+
+numb=12345    #1234  #123  #0
+rev=0    #5  #54 #54321
+
+while numb > 0:    # 1234 != 0
+    d=numb%10    #d=5  #4  #3
+    rev=rev*10+d  #0+5=5 #50+4=54  #540+3=543
+    numb=numb//10    #1234   #123  #12
+
+print('reverse numb',rev)
